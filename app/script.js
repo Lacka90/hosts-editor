5e40e3fcf133330a31dcd9873ae45ce1e9518117
Laszlo Csele <laszlo.csele90@gmail.com>
2017-03-11 00:57:17 +0100

    basic electron app to modifying hosts file with sudo

---
+++ app/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var remote = require('electron').remote;
+var dialog = remote.dialog;
+
+var sudo = require('sudo-prompt');
+var options = {
+  name: 'Electron',
+};
+
+var filePath = path.join(__dirname, 'hosts');
+var fileContent = '';
+var notExist = false;
+
+var hostsRegexp = /#HOSTS_EDITOR_START#\n((.|\n)*)\n#HOSTS_EDITOR_END#/;
+
+var wrapContent = function (content) {
+  return '#HOSTS_EDITOR_START#\n' + content + '\n#HOSTS_EDITOR_END#'
+}
+
+var tabHandler = function(e) { 
+  var keyCode = e.keyCode || e.which; 
+
+  if (keyCode == 9) {
+    e.preventDefault(); 
+    var start = document.activeElement.selectionStart;
+    var end = document.activeElement.selectionEnd;
+
+    this.value = this.value.substring(0, start)
+                + "\t"
+                + this.value.substring(end);
+
+    document.activeElement.selectionStart = 
+    document.activeElement.selectionEnd = start + 1;
+  } 
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  var btnSave = document.getElementById('save');
+  var note = document.getElementById('hosts-content');
+
+  note.addEventListener('keydown', tabHandler);
+
+  try {
+    fileContent = fs.readFileSync(filePath, 'utf-8');
+
+    var subContent = hostsRegexp.exec(fileContent);
+    if (!subContent) {
+      notExist = true;
+      note.value = '';
+    } else {
+      note.value = subContent[1];
+    }
+
+    console.log('Loaded file:' + filePath)
+  } catch (err) {
+    console.log('Error reading the file: ' + JSON.stringify(err));
+  }
+
+  btnSave.addEventListener('click', function () {
+    var content = wrapContent(note.value);
+
+    var resultContent = fileContent.replace(hostsRegexp, content);
+    if (notExist) {
+      resultContent = fileContent + '\n\n' + content;
+    }
+    fs.writeFileSync(filePath, resultContent);
+    const cmd = 'cp ' + filePath + ' ' + '/etc/hosts';
+
+    sudo.exec(cmd, options, function(error, stdout, stderr) {});
+  });
+});
